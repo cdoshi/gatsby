b5176d4ae91f843c099991920160702a4df4f576
Chiran Doshi <chirandoshi@gmail.com>
2018-08-19 00:52:55 +0530

    Added typescript options

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,17 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-typescript`,
+    {
+    resolve: "gatsby-plugin-typescript",
+    options: {
+      transpileOnly: true, // default
+      compilerOptions: {
+        target: `es5`,
+        experimentalDecorators: true,
+        jsx: `react`,
+      }, // default
+    },
+  },
     'gatsby-plugin-offline'
   ],
 }
