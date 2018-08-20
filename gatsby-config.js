90b407396e634b0fac88a70327c2ed23865c6b76
Chiran Doshi <chirandoshi@gmail.com>
2018-08-20 20:25:27 +0530

    Resolved inconsistent packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,17 +16,7 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    {
-    resolve: "gatsby-plugin-typescript",
-    options: {
-      transpileOnly: true, // default
-      compilerOptions: {
-        target: `es5`,
-        experimentalDecorators: true,
-        jsx: `react`,
-      }, // default
-    },
-  },
+    'gatsby-plugin-typescript',
     'gatsby-plugin-offline'
   ],
 }
